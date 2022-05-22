d3580f941c181a3a3213f34665b684304bd3e9e8
Michael <michaelfarquhar4@gmail.com>
2022-05-21 23:05:22 -0400

    Install and setup react-bootstrap

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,3 +1,21 @@
+import { Col, Button, Stack, InputGroup, FormControl } from 'react-bootstrap';
+
 export const Todo = () => {
-    return <div>Todo</div>;
+    return (
+        <Stack gap={3}>
+            <Col>
+                <h1>Todo List</h1>
+            </Col>
+            <InputGroup className="mb-3">
+                <FormControl
+                    placeholder="Write a new todo"
+                    aria-label="Write a new todo"
+                    aria-describedby="add-todo"
+                />
+                <Button variant="outline-secondary" id="add-todo">
+                    Add Todo
+                </Button>
+            </InputGroup>
+        </Stack>
+    );
 };
