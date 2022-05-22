9c51135ff2491fe5bc4f3ef5966de0cb95436fdb
Michael <michaelfarquhar4@gmail.com>
2022-05-21 23:02:11 -0400

    Install and setup recoil

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { RecoilRoot } from 'recoil';
+import { Todo } from './components/Todo';
+
 function App() {
-    return <div></div>;
+    return (
+        <RecoilRoot>
+            <div style={{ margin: 'auto', marginTop: '5em', maxWidth: '500px' }}>
+                <Todo />
+            </div>
+        </RecoilRoot>
+    );
 }
 
 export default App;
